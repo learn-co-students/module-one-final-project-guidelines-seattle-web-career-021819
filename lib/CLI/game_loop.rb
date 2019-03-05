a7ff15ebf4087eceb82a5d81c203bef2b5e87600
github username <github email address>
2019-03-05 14:58:08 -0800

    Accpeting answers and checking if they're correct

--- lib/CLI/game_loop.rb
+++ lib/CLI/game_loop.rb
@@ -15,15 +15,47 @@ end
 
 def question_loop
   Question.all.each do |quest|
+    binding.pry
     puts quest.question
-    answers = [
-      quest.correct,
-      quest.incorrect1,
-      quest.incorrect2,
-      quest.incorrect3
-    ].shuffle
+    answer_hash = shuffle_and_print_answers(quest)
+    puts
+    puts"Enter your answer:"
+    user_input = gets.chomp
+    #take letter or text?
+    check_answer(quest, answer_hash, user_input)
   end
 end
+
+def shuffle_and_print_answers(question)
+  answers = [
+    question.correct,
+    question.incorrect1,
+    question.incorrect2,
+    question.incorrect3
+  ].shuffle
+
+  hash = {}
+  letter = "A"
+  answers.each do |answer|
+    puts "#{letter}. #{answer}"
+    hash[letter] = answer
+    letter = letter.next
+  end
+  return hash
+end
+
+def check_answer(quest, answer_hash, user_input)
+  #track points in game_session. store correctness?
+  if answer_hash[user_input.upcase] == quest.correct
+    puts "Correct"
+    puts
+  else
+    puts "Bearly missed it."
+    puts
+  end
+end
+
+
 
 def pretty_print_question(question)
 
