fdd7bb480909c745c9623de385d04b61b782b7a9
github username <github email address>
2019-03-05 12:54:35 -0800

    Started CLI file to rub game

--- db/migrate/003_Create_Game_Sessions.rb
+++ db/migrate/003_Create_Game_Sessions.rb
@@ -1,4 +1,4 @@
-class CreateGameSessions < ActiveRecord::Migration[5.2]
+class CreateGameSessions < ActiveRecord::Migration[5.0]
   def change
     create_table :game_sessions do |t|
       t.integer :user_id
