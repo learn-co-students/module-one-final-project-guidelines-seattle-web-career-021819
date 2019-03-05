cb3ca3c8308a59cdfbb6ef208d215ba7647ad6ad
github username <github email address>
2019-03-05 13:13:11 -0800

    Restructure game loop

--- lib/CLI/game_loop.rb
+++ lib/CLI/game_loop.rb
@@ -1,4 +1,20 @@
 def start_game(user)
+  initiate_game(user)
+  question_loop
+end
+
+def initiate_game(user)
   new_game = Game.create
   new_game_session = GameSession.new(user_id: user.id, game_id: new_game.id)
+  TriviaApi.get_questions.each do |question|
+    Question.create(question)
+  end
+end
+
+def question_loop
+  Question.all.each do |quest|
+    puts quest.question
+    answers = [correct, incorrect1, incorrect2, incorrect3].scramble
+    binding.pry
+  end
 end
