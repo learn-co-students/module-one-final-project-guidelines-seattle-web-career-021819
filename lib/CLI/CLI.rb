5da240a0f9c85f212a163517dc2cb9890bdd4534
github username <github email address>
2019-03-06 11:43:54 -0800

    reformatted menus

--- lib/CLI/CLI.rb
+++ lib/CLI/CLI.rb
@@ -11,37 +11,42 @@ def welcome
   aa = Artii::Base.new :font => 'big'
   bb = Artii::Base.new :font => 'univers'
   cc = Artii::Base.new :font => 'epic'
-  puts aa.asciify('Welcome to')
-  sleep(2)
-  system "clear"
-  puts aa.asciify("Who Wants To Be A")
-  sleep(1)
-  puts bb.asciify("Millionbear").center(100)
-  puts cc.asciify("$$$").center(100)
-  sleep(2)
-  system "clear"
-  puts aa.asciify "With your host"
-  sleep(1)
-  system "clear"
-  puts bb.asciify "Alex Trebear"
-  sleep(2)
-  system "clear"
+  # puts aa.asciify('Welcome to')
+  # sleep(2)
+  # system "clear"
+  # puts aa.asciify("Who Wants To Be A")
+  # sleep(1)
+  # puts bb.asciify("Millionbear").center(100)
+  # puts cc.asciify("$$$").center(100)
+  # sleep(2)
+#   system "clear"
+#   puts aa.asciify "With your host"
+#   sleep(1)
+#   system "clear"
+#   puts bb.asciify "Alex Trebear"
+#   sleep(2)
+#   system "clear"
+#   bear_host
 end
-
+#
 def get_user
-  puts "What's your name?"
+  dd= Artii::Base.new :font => 'invita'
+  puts
+  puts dd.asciify("What's your name?")
   new_name = gets.chomp
   puts
+  system "clear"
   new_user = User.create(name: new_name)
   new_user
 end
 
 
 def menu(user)
-  puts "What would you like to do?"
+  aa = Artii::Base.new :font => 'small'
+  puts aa.asciify("What would you like to do?")
   puts
-  puts "1. Play a new game"
-  puts "2. Exit"
+  puts aa.asciify("1. Play a new game")
+  puts aa.asciify("2. Exit")
   user_input = gets.chomp
   if user_input == "1"
     start_game(user)
