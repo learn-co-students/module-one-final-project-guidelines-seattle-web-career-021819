b9887183668dfd1aabea590f8caa312f7b3e645b
Reid Boudreau <rtboudreau@gmail.com>
2019-03-07 10:24:10 -0800

    Updates to cli files

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,15 @@
 require_relative '../config/environment'
 
+
+
 class CLI
+
+  @@input = ""
+  def self.input
+    @@input
+  end
+
+
     def self.run
         puts "
 
@@ -32,7 +41,7 @@ class CLI
         is_running = true
         while is_running
 
-          # self.get_user
+          self.get_user
 
           self.main_menu
             choice = STDIN.gets.chomp
@@ -40,9 +49,9 @@ class CLI
             if choice == "0"
                 is_running = false
             # echo what the user types
-            elsif choice == "1"
+            elsif choice == "1"  || choice == "one"
                 self.easy_game
-            elsif choice == "2"
+            elsif choice == "2"  || choice == "two"
                 self.hard_game
             else
                 puts "\n[Please enter in option 1, 2, or 3]"
@@ -50,33 +59,41 @@ class CLI
         end
     end
 
-    def self.get_user
-      puts "\nWelcome! \nWho may I ask is playing today?".red
-      input = STDIN.gets.chomp
-      new_user = User.create_user(input)
-      new_user
-    end
 
     def self.main_menu
-        puts "\nHi, #{get_user.name.upcase}! would you like to guess some cocktail names based solely on their ingredients?".red
+        puts "\nHi, #{@@input.upcase}! would you like to guess some cocktail names based solely on their ingredients?".red
         puts "\n[0] Nope. exit".red
         puts "\n[1] Sure, but lets play on easy mode".red
         puts "\n[2] I consider myself a connisuer.  Let's play on hard mode.".red
     end
 
+    def self.get_user
+      puts "\nWelcome! \nWho may I ask is playing today????".red
+      @@input = STDIN.gets.chomp
+      new_user = User.create_user(input)
+      new_user
+    end
+
+
+
     def self.easy_game
+      puts "hello"
         Game.create_game
-        Round.create_round
+        var = Round.create_round
+        binding.pry
+        puts "#{Round.question}"
+        Round.choices { |choice| puts "#{choice}" }
+
     end
 
-    def self.hard_game
+    # def self.hard_game
+    #
+    # end
 
-    end
 
 
-
-    def self.get_json(url)
-        response = RestClient.get(url)
-        json = JSON.parse(response.body)
-    end
+    # def self.get_json(url)
+    #     response = RestClient.get(url)
+    #     json = JSON.parse(response.body)
+    # end
 end
