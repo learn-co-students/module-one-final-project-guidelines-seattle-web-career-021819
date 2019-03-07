61063b7b83e7a1c81bc11912e0a6be42c812bf83
Joshua Loran <joshua.loran@gmail.com>
2019-03-06 21:06:00 -0800

    added character name lookup

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -34,30 +34,35 @@ end
 	end
 
 	def self.character
-		puts "Enter an id for a Character to lookup:"
+		puts "Enter a name for the Character to lookup:"
 		id = STDIN.gets.chomp
+		found_character = Character.find_by(name: id)
+		if found_character != nil
+			puts found_character.name
+		else
+			puts "Nobody matches that name"
+			puts "Please enter a valid name"
+		end
 
-		#performing network request
-		url = GAME_OF_THRONES_URL+ "characters/#{id}"
-		json=url
+
 
 		#print out name of the character
 		# ***NOT WORKING****
-		name = json["name"]
-		puts "Character: #{name}"
+		# name = json["name"]
+		# puts "Character: #{name}"
 
 		#prompt the user to see if we want to lookup the seasons of GOT
 		#they were featured in
-		puts "Would you like to see the seasons #{name} was featured in? (yes/no)"
+		# puts "Would you like to see the seasons #{name} was featured in? (yes/no)"
 
 		#if user says yes! print the array of seasons the character was featured
-		choice = STDIN.gets.chomp
-		if choice.starts_with? "y" || choice == "yes"
-			json = self.get_json(json["homeworld"])
-			tvSeries=json["tvSeries"]
-			puts "Seasons featured: #{tvSeries}"
-			puts
-		end
+		# choice = STDIN.gets.chomp
+		# if choice.starts_with? "y" || choice == "yes"
+		# 	json = self.get_json(json["homeworld"])
+		# 	tvSeries=json["tvSeries"]
+		# 	puts "Seasons featured: #{tvSeries}"
+		# 	puts
+		# end
 	end
 
 
